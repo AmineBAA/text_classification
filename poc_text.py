a7e7fbd28d52e7bba850e8e1a0472413e4f1b62f
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2024-05-18 00:06:51 +0100

    poc_text.py

--- poc_text.py
+++ poc_text.py
@@ -24,7 +24,7 @@ import pandas as pd
 import pickle
 
 # Load your trained model
-classifier, vectorizer = pickle.load(open('C:/Users/AMINE/Downloads/text_classifier.pkl', 'rb'))
+classifier, vectorizer = pickle.load(open('https://github.com/AmineBAA/text_classification/blob/main/text_classifier.pkl', 'rb'))
 
 st.title('Text Classification Tool')
 
