60190cb07ad251802a49cd6518cd536523b187dc
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2024-05-18 08:52:26 +0100

    Update poc_text.py

--- poc_text.py
+++ poc_text.py
@@ -8,10 +8,6 @@ Original file is located at
 """
 
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
 import re
 
