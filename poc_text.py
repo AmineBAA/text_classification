10bc7cec69b9fd95112f97ba1beee7dc589bc780
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2024-05-20 14:40:50 +0100

    Update poc_text.py

--- poc_text.py
+++ poc_text.py
@@ -45,7 +45,6 @@ if uploaded_file is not None:
     X_test_vect = vectorizer.transform(X_test)
     predictions = classifier.predict(X_test_vect)  # adjust the column name
     df_test['predictions'] = predictions
-    df_test['predicted_label']=le.inverse_transform(predictions)
     st.write(df_test)
 
 
