20b26e73085950e45e42bcfeb8638c783ea9d1e8
Amine BAAZZOUZ <141878518+AmineBAA@users.noreply.github.com>
2024-05-20 14:32:17 +0100

    Update poc_text.py

--- poc_text.py
+++ poc_text.py
@@ -45,10 +45,12 @@ if uploaded_file is not None:
     X_test_vect = vectorizer.transform(X_test)
     predictions = classifier.predict(X_test_vect)  # adjust the column name
     df_test['predictions'] = predictions
-    if predictions==1:
-        df_test['predicted_label'] = 'autre'
-    else:
-        df_test['predicted_label'] = 'cards'
+    for i in range(0,predictions.shape[1]):
+        if predictions==1:
+           df_test['predicted_label'][i] = 'autre'
+        else:
+           df_test['predicted_label'][i] = 'cards'
+    
     st.write(df_test)
 
 
